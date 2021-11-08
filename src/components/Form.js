be7fddaf87af6894dc8b1767aff80574473a5635
mckinseyleckenby <leckenbym11@gmail.com>
2021-11-08 07:08:17 -0600

    commit tree 4efbd5afb53d86987aa0d9b01d58029492a4dd02

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
 
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -11,13 +12,42 @@ function Form() {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+  function handleSubmit(e) {
+    e.preventDefault()
+    //below we are putting together form into an object
+    //using values stored in state
+    const formData = {
+      firstName : firstName,
+      lastName : lastName,
+    };
+    const dataArray = [...submittedData, formData];
+   setSubmittedData (dataArray);
+   setFirstName('');
+   setLastName('');
+  }
+    //a form, when submitted, should send the form data somewhere
+    //in react we handle wit async javascript
+    //so think of sendDataSomewhere as the code that handles sending this data off
+    //this function can be defined in same form component OR can be passed down as prop
+const listOfSubmissions = submittedData.map((data, index) => {
+  return (
+    <div key={index}>
+      {data.firstName} {data.lastName}
+    </div>
+  )
+})
+
 
   return (
-    <form>
+    <div>
+    <form onSubmit = {handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    <h3>Submissions</h3>
+    {listOfSubmissions}
+    </div>
   );
 }
 
